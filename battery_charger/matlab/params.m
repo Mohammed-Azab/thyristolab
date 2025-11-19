2dce1e43720ea88e0e38377609947afc04868c0c
Mohammed Azab <Mo7ammed3zab@outlook.com>
2025-11-20 00:43:52 +0100

    feat: add missing parameters for battery charger simulation and visualization

--- battery_charger/matlab/params.m
+++ battery_charger/matlab/params.m
@@ -9,6 +9,7 @@ Vbat = 12;
 Rbat = 0.1; 
 capacity = 50; 
 capUnit = 'Ah';
+SoC_target = 100;
 
 % SoC & time
 SoC_init = 20; 
@@ -16,7 +17,16 @@ SoC_target = 80;
 t_charge = [];
 
 % Thyristor
+alpha_deg = 0:5:175; 
 Vt = 0; 
+Rth = 0;
 Ileak = 0; 
 t_rise = 0; 
 t_fall = 0;
+
+% Simulation params 
+dt = 1/(300*f);
+
+% Visualization
+enablePlots = true;
+alpha = 30;
