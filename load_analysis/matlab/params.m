c66b9bf9a821c83b8f3be9bbf48e25d21098edcd
Mohammed Azab <Mo7ammed3zab@outlook.com>
2025-12-06 21:15:30 +0100

    Refactor params.m to streamline load scenarios and remove unused battery parameters

--- load_analysis/matlab/params.m
+++ load_analysis/matlab/params.m
@@ -3,31 +3,27 @@
 % Supply
 Vrms = 230; 
 f = 50;
+alphas_deg = 0:5:180;            
+simTime = 0.1;
+T = 1/f;
 
-% Battery
-Vbat = 20; 
-Rbat = 0.1; 
-capacity = 50; 
-capUnit = 'Ah';
+% Load scenarios
+scenarios = struct();
+scenarios(1).name = 'resistive_only';
+scenarios(1).R = 10;           % Resistance only (ohms)
+scenarios(1).L = 1e-6;         % Minimal inductance (H)
 
-% SoC & time
-SoC_init = 12; 
-SoC_target = 100;
-t_charging_hours =  0.5; % Simulated User Input
-t_charge = inf;
+scenarios(2).name = 'R_L_load';
+scenarios(2).R = 10;           % Resistance (ohms)
+scenarios(2).L = 50e-3;        % Moderate inductance (H)
 
-% Thyristor
-alpha = 30;
-alpha_deg = 0:2:180;         
-Vt = 1.5;       
-Rth = 0.001;  
-Ileak = 0.01;  
-t_rise = 1e-6;  
-t_fall = 2e-6;  
+scenarios(3).name = 'highly_inductive';
+scenarios(3).R = 5;            % Lower resistance (ohms)
+scenarios(3).L = 200e-3;       % High inductance (H)
 
-% Simulation params 
-dt = 1/(300*f);
+% Pulse Generator parameters
+pulse_amplitude = 5;           % Gate signal amplitude (V)
+pulse_width = 50;              % Pulse width (% of period)
+pulse_period = 1/f;            % Period based on line frequency (s)
+     
 
-% Visualization
-enablePlots = true;
-savePlots = false; 
